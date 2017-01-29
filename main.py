beb7f88f675f222e70213d4f130b25bb1abb25bd
Himura Kazuto <glagol15@gmail.com>
2017-01-29 20:43:46 +0300

    GUI begun

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
-#!/usr/bin/env python
+#! python3
 
-import time
+import tkinter
+from tkinter import ttk
 from pirc522 import RFID
+
+
+class Application(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack(fill='both', expand=True)
+
+        self.master.title("MFRC522 Tool")
+        self.master.minsize(200, 80)
+        self.master.geometry("500x300")
+
+        self.w_toolbar = ttk.Frame(self)
+
+        self.ports_list = ["one", 'two']
+        self.port = None
+        self.w_port = ttk.Combobox(self.w_toolbar, values=self.ports_list, textvariable=self.port)
+        self.w_port.current(0)
+
+        self.w_dump = ttk.Button(self.w_toolbar, text="Dump", command=self.dump)
+        self.w_connect = ttk.Button(self.w_toolbar, text="Connect", command=self.connect)
+        self.w_in = ttk.Entry(self.w_toolbar)
+
+        self.w_out = tkinter.Text(self, height=1)
+        self.w_progressbar = ttk.Progressbar(self)
+
+        # Layouting
+
+        self.w_port.pack(side='left')
+        self.w_connect.pack(side='left')
+        self.w_in.pack(side='left', fill='x', expand=True)
+        self.w_dump.pack(side='left')
+
+        self.w_toolbar.pack(side='top', fill="x")
+        self.w_out.pack(fill='both', expand=True)
+        self.w_progressbar.pack(side="bottom", fill="x")
+
+    def connect(self):
+        print("connect to", self.port)
+
+    def dump(self):
+        print("dump", self.port)
+
+root = tkinter.Tk()
+app = Application(master=root)
+app.mainloop()
+
+exit()
+
+
+
 
 rdr = RFID()
 util = rdr.util()
